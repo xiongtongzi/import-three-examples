c0e8f9cfda6a21ec62e9d635ee3155317e8813a9
kuangyazhou@tyche.net.cn <kuangyazhou@tyche.net.cn>
2019-06-03 09:36:41 +0800

    fix addMethod

--- src/addMethod.js
+++ src/addMethod.js
@@ -1,25 +1,29 @@
 /*
-*   add methods
-*   time: 2018/12/29
-*   author: xiongtongzi
-* */
+ *   add methods
+ *   time: 2019/06/03
+ *   author: kuangyazhou
+ * */
 
-function addBase (path, arr) {
-    let a  = []
+function addBase(path, arr) {
+    let a = []
     arr.forEach((v, i) => {
-        a.push({
-            test: require.resolve(`${path}${v}`),
-            use: `imports-loader?THREE=three`
-        })
-        a.push({
-            test: require.resolve(`${path}${v}`),
-            use: `exports-loader?THREE.${v}`
-        })
+        try {
+            a.push({
+                test: require.resolve(`${path}${v}`),
+                use: `imports-loader?THREE=three`
+            })
+            a.push({
+                test: require.resolve(`${path}${v}`),
+                use: `exports-loader?THREE.${v}`
+            })
+        } catch (e) {
+
+        }
     })
     return a
 }
 
-function recursion (arr, use, path, target) {
+function recursion(arr, use, path, target) {
     arr.forEach((v, i) => {
         use += `,${v.name}=${v.path}`
         if (v.install && v.install.length > 0) {
@@ -32,29 +36,41 @@ function recursion (arr, use, path, target) {
     })
 }
 
-function addSpecial (arr) {
+function addSpecial(arr) {
     var a = []
     arr.forEach((v, i) => {
         if (v.lib && v.lib.length > 0) {
             v.lib.forEach((v1, i1) => {
-                a.push({
-                    test: require.resolve(`${v.path}`),
-                    use: `imports-loader?${v1.name}=${v1.path}`
-                })
+                try {
+                    a.push({
+                        test: require.resolve(`${path}${v}`),
+                        use: `imports-loader?${v1.name}=${v1.path}`
+                    })
+                } catch (e) {
+
+                }
             })
         }
         if (v.install && v.install.length > 0) {
             recursion(v.install, `imports-loader?THREE=three`, v.path, a)
         } else {
-            a.push({
-                test: require.resolve(`${v.path}`),
-                use: "imports-loader?THREE=three"
-            })
+            try {
+                a.push({
+                    test: require.resolve(`${path}${v}`),
+                    use: "imports-loader?THREE=three"
+                })
+            } catch (e) {
+
+            }
         }
-        a.push({
-            test: require.resolve(`${v.path}`),
-            use: `exports-loader?THREE.${v.name}`
-        })
+        try {
+            a.push({
+                test: require.resolve(`${path}${v}`),
+                use: `exports-loader?THREE.${v.name}`
+            })
+        } catch (e) {
+
+        }
     })
     return a
 }
